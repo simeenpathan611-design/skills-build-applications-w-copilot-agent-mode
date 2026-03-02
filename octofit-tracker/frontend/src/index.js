18a6f5f70b9a21166c1e13c6805f8d090ef8a96d
Simeen Pathan <simeenpathan611@gmail.com>
2026-03-02 12:37:28 +0000

    ss

--- octofit-tracker/frontend/src/index.js
+++ octofit-tracker/frontend/src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// Bootstrap CSS
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
